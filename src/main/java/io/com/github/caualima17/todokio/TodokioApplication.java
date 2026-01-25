49fbf720f757126ee40fc036d92e30afb134a695
Cauã de Lima <caua-l1@outlook.com>
2026-01-25 18:18:05 -0300

    chore: configuração inicial do ambiente spring

---
+++ src/main/java/io/com/github/caualima17/todokio/TodokioApplication.java
@@ -0,0 +1,13 @@
+package io.com.github.caualima17.todokio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodokioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodokioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/com/github/caualima17/todokio/TodokioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.com.github.caualima17.todokio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodokioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
